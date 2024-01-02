0523f0b2d8f77fb20fd47906a1179998de423ddb
Thuany Mendonca <thuany1997@hotmail.com>
2024-01-02 10:59:41 -0300

    example with mvc

---
+++ teste/Models/Teste.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace teste.Models
+{
+	public class Teste
+	{
+		public int ID { get; set; }
+		public string Titulo { get; set; } = string.Empty;
+
+		[DataType(DataType.Date)]
+		public DateTime DataLancamento { get; set; }
+		public string Genero { get; set; } = string.Empty;
+		public decimal Preco { get; set; }
+		
+	}
+}
